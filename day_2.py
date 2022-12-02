342c2317441a1e114e89469e0a98fe4220336df5
Alexander Alasjö <alexander@alasjo.se>
2022-12-02 07:36:26 +0100

    Day 2

---
+++ day_2.py
@@ -0,0 +1,71 @@
+'''
+--- Day 2: Rock Paper Scissors ---
+
+1. What would your total score be if everything goes exactly according to your strategy guide?
+2. Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+'''
+
+from santas_helpers import file_lines
+
+rps_winner = {
+  'R': 'S',
+  'P': 'R',
+  'S': 'P'
+}
+
+rps_score_table = {
+  'R': 1, 'P': 2, 'S': 3,
+  'W': 6, 'L': 0, 'D': 3
+}
+
+
+def rps_score(elf_a, elf_b):
+  # Score for participating (chosen play)
+  s = rps_score_table[elf_b]
+
+  if elf_a == elf_b:
+    # DRAW
+    return rps_score_table['D'] + s
+  
+  if rps_winner[elf_a] == elf_b:
+    # LOSE
+    return rps_score_table['L'] + s
+
+  if rps_winner[elf_b] == elf_a:
+    # WIN
+    return rps_score_table['W'] + s
+
+  # hmm
+  exit(1)
+
+
+def parse_rps(s: str):
+  return s.strip()
+
+
+score_strategy_a = 0
+score_strategy_b = 0
+
+for line in file_lines("./input_2.txt", parser=parse_rps):
+  # Strategy A
+  line = line.replace('A', 'R').replace('X', 'R')
+  line = line.replace('B', 'P').replace('Y', 'P')
+  line = line.replace('C', 'S').replace('Z', 'S')
+  elf_a, elf_b = line.split(' ')
+  score_strategy_a += rps_score(elf_a, elf_b)
+
+  # Strategy B
+  if elf_b == 'R':
+    # LOSE
+    score_strategy_b += rps_score(elf_a, rps_winner[elf_a])
+  elif elf_b == 'P':
+    # DRAW
+    score_strategy_b += rps_score(elf_a, elf_a)
+  elif elf_b == 'S':
+    # WIN
+    winner = list(rps_winner.keys())
+    loser = list(rps_winner.values())
+    score_strategy_b += rps_score(elf_a, winner[loser.index(elf_a)])
+
+print(f"Part 1: {score_strategy_a}")
+print(f"Part 2: {score_strategy_b}")
